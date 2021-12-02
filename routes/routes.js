847a9f898a17fcf42e8bc65da60c5d271cfbbd52
muell <muell758@d.umn.edu>
2021-12-02 14:07:46 -0600

    Update API message

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ let router = express.Router()
 
 // req -- request from client, res -- response from server
 router.get('/', function(req, res, next) {
-    res.json( { 'message': 'Hello ITEC 2560 web programmers!'} )
+    res.json( { 'message': 'Hello AWESOME ITEC 2560 web programmers!'} )
 })
 
 module.exports = router
