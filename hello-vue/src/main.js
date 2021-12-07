c2365222794083b67471379bb057fd1f644a5d67
muell <muell758@d.umn.edu>
2021-12-07 15:00:57 -0600

    fixed hello message that wasn't showing

--- hello-vue/src/main.js
+++ hello-vue/src/main.js
@@ -6,4 +6,9 @@ let app = createApp(App)
 
 app.config.globalProperties.$hello_api = HelloAPI
 
-createApp(App).mount('#app')
+// this creates a new app, without hello_api which it 
+// uses for the #app div on the page
+// createApp(App).mount('#app')
+
+// Instead, this is using the app created on line 5
+app.mount('#app')
